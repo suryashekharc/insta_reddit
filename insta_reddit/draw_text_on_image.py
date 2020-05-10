bdc71c28d2b558b615947576898e7f902d7c6dad
Surya Shekhar Chakraborty <suryaschak@gmail.com>
2020-05-10 16:58:37 +0530

    Enable containerized vertical centering of textbox

--- insta_reddit/draw_text_on_image.py
+++ insta_reddit/draw_text_on_image.py
@@ -1,8 +1,10 @@
 import os
 from insta_reddit.image_utils import ImageText
+
+
 # Download and install fonts from: https://www.cufonfonts.com/font/helvetica-neue-9
 
-def get_title_and_self_text():
+def get_title_and_self_text():  # Ensure title < 400 characters, self_text < 830 characters
     title = " Get in the habit of playing Rock Paper Scissors with people, but make sure you " \
             "consistently lose with the same throw. Eventually they will learn to expect the same " \
             "throw and you can win any game you actually care about."
@@ -29,23 +31,55 @@ def get_format():
     return {'subreddit_font': 'Helvetica95Black.ttf',
             'title_font': 'Helvetica65Medium_22443.ttf',
             'self_text_font': 'Helvetica55Roman_22439.ttf',
-            'subreddit_color': (159,4,4),
-            'title_color': (33,32,32),
-            'self_text_color':(0,0,0)}
+            'subreddit_color': (159, 4, 4),
+            'title_color': (33, 32, 32),
+            'self_text_color': (0, 0, 0)}
 
 
 def write_on_img():
     title, self_text = get_title_and_self_text()
     img_1 = get_bg_img()
     img_2 = get_bg_img()
-    # TODO: Figure out how to vertically align centrally as well
-    img_1.write_text_box((50, 20), text="ULPT:", box_width=400, font_filename=get_format()['subreddit_font'],
-                       font_size=60, color=get_format()['subreddit_color'], place='center')
-    img_1.write_text_box((50, 140), title, box_width=400, font_filename=get_format()['title_font'],
-                       font_size=15, color=get_format()['title_color'], place='justify')
-    img_2.write_text_box((50, 50), self_text, box_width=400, font_filename=get_format()['self_text_font'],
-                       font_size=12, color=get_format()['self_text_color'], place='justify')
+    img_3 = get_bg_img()
+    img_4 = get_bg_img()
+    img_5 = get_bg_img()
+    img_1.write_text_box((50, 100), text="ULPT:", box_width=400,
+                         font_filename=get_format()['subreddit_font'],
+                         font_size=60, color=get_format()['subreddit_color'], place='center')
+    img_1.write_text_box((50, 200), title, box_width=400, font_filename=get_format()['title_font'],
+                         font_size=20, color=get_format()['title_color'], place='justify')
+    img_2.write_vertically_centred_text_box(left_padding=50, upper=100, lower=400,
+                                            text=self_text, box_width=400,
+                                            font_filename=get_format()['self_text_font'],
+                                            font_size=20, color=get_format()['self_text_color'],
+                                            place='justify')
+    img_3.write_vertically_centred_text_box(left_padding=50, upper=0, lower=250, text="ULPT:",
+                                            box_width=400,
+                                            font_filename=get_format()['subreddit_font'],
+                                            font_size=60, color=get_format()['subreddit_color'],
+                                            place='center')
+    img_3.write_vertically_centred_text_box(left_padding=50, upper=150, lower=450, text=title,
+                                            box_width=400,
+                                            font_filename=get_format()['title_font'],
+                                            font_size=20, color=get_format()['title_color'],
+                                            place='justify')
+    img_4.write_text_box((50, 100), text="ULPT:", box_width=400,
+                         font_filename=get_format()['subreddit_font'],
+                         font_size=60, color=get_format()['subreddit_color'], place='center')
+    img_4.write_text_box((50, 200), title[:150], box_width=400,
+                         font_filename=get_format()['title_font'],
+                         font_size=20, color=get_format()['title_color'], place='justify')
+    img_5.write_text_box((50, 80), text="ULPT:", box_width=400,
+                         font_filename=get_format()['subreddit_font'],
+                         font_size=60, color=get_format()['subreddit_color'], place='center')
+    img_5.write_text_box((50, 200), title + title[:185], box_width=400,
+                         font_filename=get_format()['title_font'],
+                         font_size=20, color=get_format()['title_color'], place='justify')
     img_1.save('sample-imagetext_1.png')
     img_2.save('sample-imagetext_2.png')
+    img_3.save('sample-imagetext_3.png')
+    img_4.save('sample-imagetext_4.png')
+    img_5.save('sample-imagetext_5.png')
+
 
 write_on_img()
--- insta_reddit/image_utils.py
+++ insta_reddit/image_utils.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# Copyright 2011 Álvaro Justen [alvarojusten at gmail dot com]
-# License: GPL <http://www.gnu.org/copyleft/gpl.html>
-
 from PIL import Image, ImageDraw, ImageFont
 
 
@@ -84,7 +78,6 @@ class ImageText(object):
         lines = [' '.join(line) for line in lines if line]
         height = y
         for index, line in enumerate(lines):
-            height += text_height
             if place == 'left':
                 self.write_text((x, height), line, font_filename, font_size,
                                 color)
@@ -121,4 +114,87 @@ class ImageText(object):
                 last_word_x = x + box_width - last_word_size[0]
                 self.write_text((last_word_x, height), words[-1], font_filename,
                                 font_size, color)
+            height += text_height
+        return box_width, height - y
+
+    def write_vertically_centred_text_box(self, left_padding, upper, lower, text, box_width,
+                                          font_filename,
+                                          font_size=11, color=(0, 0, 0), place='left',
+                                          justify_last_line=False):
+        """
+        Create a textbox which is vertically centred within the limits of a container in the image
+        :param left_padding:        Pixels to leave on the left side before textbox begins
+        :param upper:               Upper pixel index to start the vertical centralizing container
+        :param lower:               Lower pixel index to end the vertical centralizing container
+        :param text:                Text to write on the image
+        :param box_width:           How wide the textbox will be
+        :param font_filename:       Font file name with .ttf suffix
+        :param font_size:           Size of the font
+        :param color:               Color of the font
+        :param place:               Text alignment: left, write, center, justify.
+        :param justify_last_line:   Whether to justify the last line
+        :return:                    Dimensions of the textbox (width, height)
+        """
+        x = left_padding
+        lines = []
+        line = []
+        words = text.split()
+        for word in words:
+            new_line = ' '.join(line + [word])
+            size = self.get_text_size(font_filename, font_size, new_line)
+            text_height = size[1]
+            if size[0] <= box_width:
+                line.append(word)
+            else:
+                lines.append(line)
+                line = [word]
+        if line:
+            lines.append(line)
+        lines = [' '.join(line) for line in lines if line]
+
+        # Calculate y here
+        height_of_text = len(lines) * text_height
+        mid_section = ((upper + lower) / 2)
+        y = mid_section - (height_of_text / 2)
+        # End of calculate y here
+
+        height = y
+        for index, line in enumerate(lines):
+            if place == 'left':
+                self.write_text((x, height), line, font_filename, font_size,
+                                color)
+            elif place == 'right':
+                total_size = self.get_text_size(font_filename, font_size, line)
+                x_left = x + box_width - total_size[0]
+                self.write_text((x_left, height), line, font_filename,
+                                font_size, color)
+            elif place == 'center':
+                total_size = self.get_text_size(font_filename, font_size, line)
+                x_left = int(x + ((box_width - total_size[0]) / 2))
+                self.write_text((x_left, height), line, font_filename,
+                                font_size, color)
+            elif place == 'justify':
+                words = line.split()
+                if (index == len(lines) - 1 and not justify_last_line) or \
+                        len(words) == 1:
+                    self.write_text((x, height), line, font_filename, font_size,
+                                    color)
+                    continue
+                line_without_spaces = ''.join(words)
+                total_size = self.get_text_size(font_filename, font_size,
+                                                line_without_spaces)
+                space_width = (box_width - total_size[0]) / (len(words) - 1.0)
+                start_x = x
+                for word in words[:-1]:
+                    self.write_text((start_x, height), word, font_filename,
+                                    font_size, color)
+                    word_size = self.get_text_size(font_filename, font_size,
+                                                   word)
+                    start_x += word_size[0] + space_width
+                last_word_size = self.get_text_size(font_filename, font_size,
+                                                    words[-1])
+                last_word_x = x + box_width - last_word_size[0]
+                self.write_text((last_word_x, height), words[-1], font_filename,
+                                font_size, color)
+            height += text_height  # moved this to the end else a
         return box_width, height - y
